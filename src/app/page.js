cf3863841c29f258998a96fb98e3e7d096f9b025
gauravkumarit8 <gaurav.kumarit8@gmail.com>
2024-06-02 13:21:20 +0530

    All post added to home

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import AllPost from "./(blog)/allPost/page";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Welcome to blog application</h1>
+      <AllPost />
     </main>
   );
 }
--- src/utils/createBlogPost/createBlog.js
+++ src/utils/createBlogPost/createBlog.js
@@ -1,9 +1,21 @@
-export default async function CreateBlog(title,body){
-    const response=await fetch("",{
-        method:'Post',
-        headers:{
-            'Context-Type':'application/json',
-        }
-    })
-
+export async function uploadProduct(productData) {
+    try {
+      const response = await fetch('https://fakestoreapi.com/products', {
+        method: 'POST',
+        body: JSON.stringify(productData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (!response.ok) {
+        throw new Error('Failed to upload product');
+      }
+  
+      const result = await response.json();
+      return { success: true, data: result };
+    } catch (error) {
+      return { success: false, message: error.message };
+    }
 }
+  
